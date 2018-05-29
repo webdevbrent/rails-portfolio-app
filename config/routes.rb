6c85a82882d641efcb9cd59e3d03ee756f8a2b62
webdevbrent <webdeveloperbrent@gmail.com>
2018-05-29 10:38:19 -0400

    Added toggle action for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts, except: [:show, :index]
-  get 'blog', to: 'posts#index'
-  get 'blog/:id', to: 'posts#show', as: 'blog_show'
+
+  resources :posts do
+    member do
+      get :toggle_status
+    end
+  end
+
+  # resources :posts, except: [:show, :index]
+  # get 'blog', to: 'posts#index'
+  # get 'blog/:id', to: 'posts#show', as: 'blog_show'
 
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
