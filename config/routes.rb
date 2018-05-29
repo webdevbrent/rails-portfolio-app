80912a53633155797256fdf7522596f55d5144d3
webdevbrent <webdeveloperbrent@gmail.com>
2018-05-29 08:53:50 -0400

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :portfolios
+
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
