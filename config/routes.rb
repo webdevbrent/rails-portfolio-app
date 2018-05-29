5b1f762e2b19a8af7dc4cd1ee1b4d8f019e0952f
webdevbrent <webdeveloperbrent@gmail.com>
2018-05-29 11:58:37 -0400

    Changed routes to blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :posts do
+  get 'blog', to: 'posts#index', as: 'blog'
+  get 'blog/:id', to: 'posts#show', as: 'blog_show'
+  resources :posts, except: [:index, :show] do
     member do
       get :toggle_status
     end
   end
 
-  # resources :posts, except: [:show, :index]
-  # get 'blog', to: 'posts#index'
-  # get 'blog/:id', to: 'posts#show', as: 'blog_show'
+
 
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
