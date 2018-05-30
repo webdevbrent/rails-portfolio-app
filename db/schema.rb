bc0c32c44ebf5856738162902ed13e9c41b9265b
webdevbrent <webdeveloperbrent@gmail.com>
2018-05-30 07:50:17 -0400

    Implemented validations for blogs skills and portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -47,16 +47,6 @@ ActiveRecord::Schema.define(version: 20180529160859) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug"
-    t.integer "status", default: 0
-    t.index ["slug"], name: "index_posts_on_slug", unique: true
-  end
-
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
