1e9c90d3c42450ec69ff36f6ef2d0b2b5e1f0b7f
webdevbrent <webdeveloperbrent@gmail.com>
2018-06-03 10:35:27 -0400

    Added custom layouts for blogs and portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
     before_action :set_post, only: [:show, :edit, :update, :destroy]
+    layout "portfolio"
 
     def index
         @portfolio_items = Portfolio.all
