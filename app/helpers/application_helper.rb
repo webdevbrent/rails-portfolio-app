9f909ed9adce5ded1deedc14fbd9cdc76f660eaf
webdevbrent <webdeveloperbrent@gmail.com>
2018-06-05 10:14:29 -0400

    Design recovery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-    def login_helper
+    def login_helper(style = '')
      if current_user.is_a?(GuestUser)
-        (link_to 'Register', new_user_registration_path) +
-        "<br>".html_safe +
-        (link_to 'Login', user_session_path)    
+        (link_to 'Register', new_user_registration_path, class: 'btn btn-info') +
+        " ".html_safe +
+        (link_to 'Login', user_session_path, class: style)    
      else
-        link_to 'Logout', destroy_user_session_path, method: :delete, data: { confirm: 'Are you sure?' }
+        link_to 'Logout', destroy_user_session_path, method: :delete, data: { confirm: 'Are you sure?' }, class: style
         end
     end
 
