bbfc8462564e6c21a9c659cd068c76b410d8d45b
webdevbrent <webdeveloperbrent@gmail.com>
2018-06-07 11:22:12 -0400

    Implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,41 @@ module ApplicationHelper
     def copyright_generator
         WebdevbrentViewTool::Renderer.copyright 'Brent Phillips', 'All rights reserved'
     end
+
+    def nav_items
+      [
+        {
+          url: about_me_path,
+          title: 'About Me'
+        },
+        {
+          url: contact_path,
+          title: 'Contact'
+        },
+        {
+          url: blog_index_path,
+          title: 'Blog'
+        },
+        {
+          url: portfolios_path,
+          title: 'Portfolio'
+        }
+      ]
+    end
+
+    def nav_helper style
+      nav_links = ''
+
+      nav_items.each do |item|
+        nav_links << "<a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a>"
+      end
+
+        nav_links.html_safe
+    end
+
+    def active?(path)
+        "active" if current_page? path
+    end
+
+
 end
